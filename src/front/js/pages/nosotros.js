356932616be871d73ad4c030f8b267c84bf4c738
SPaganoDoval <dovalsonia@gmail.com>
2023-04-08 21:18:46 +0000

    equipo

---
+++ src/front/js/pages/nosotros.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const Nosotros = (){
+    <div><strong>Somos un equipo compuesto por tres personas, con alma aventurera en busqueda de nuevos desafios estando en contacto con la naturaleza por esta razon dicidimos enfocarnos a un proyecto de este tipo y queremos darles a conocer 
+    Misión:
+Organizar eventos deportivos de alta calidad que fomenten el deporte, la salud y la diversión, y que ofrezcan experiencias únicas e inolvidables para todos los participantes.
+
+Visión:
+Ser reconocidos como la mejor empresa de organización de eventos deportivos en la región, destacándonos por la calidad de nuestros eventos y la satisfacción de nuestros clientes.
+
+Valores:
+
+Pasión por el deporte: Nos apasiona el deporte y creemos en su capacidad para mejorar la calidad de vida de las personas.
+Excelencia: Buscamos la excelencia en todo lo que hacemos, desde la organización de los eventos hasta la atención al cliente.
+Compromiso: Estamos comprometidos con nuestros clientes y con la comunidad en general, y trabajamos con responsabilidad y dedicación para ofrecerles los mejores eventos posibles.
+Innovación: Buscamos constantemente nuevas formas de mejorar nuestros eventos y de ofrecer experiencias únicas e innovadoras a nuestros participantes.
+Integridad: Nos guiamos por los más altos estándares de ética y honestidad en todas nuestras relaciones comerciales y personales.</strong></div>
+
+}
